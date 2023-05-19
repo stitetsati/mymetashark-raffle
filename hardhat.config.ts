18e5530f3d8b034160a43169a678ffe92f0c1e61
loodaobao <warren.y.cheng@gmail.com>
2023-05-20 07:19:17 +0800

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-verify";
+import "@typechain/hardhat";
+import "hardhat-deploy";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      // forking: {
+      // url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      // url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      // blockNumber: 28919813, // for stable mainnet fork test
+      // },
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.TEST_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.MAINNET_PRIVATE_KEY}`],
+    },
+    ftmTestnet: {
+      url: `https://rpc.ankr.com/fantom_testnet`,
+      accounts: [`${process.env.TEST_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: `${process.env.ETHERSCAN_API_KEY}`,
+      goerli: `${process.env.ETHERSCAN_API_KEY}`,
+    },
+  },
+};
+
+export default config;
